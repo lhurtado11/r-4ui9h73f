d20dd38ca8fdccbdc59eec27fa5c7f9a88925981
Luisa <lhurtado372@gmail.com>
2021-05-12 18:29:49 -0500

    reto repetidor con componente función superado

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
+import { useState } from "react";
 import './App.css';
 
-class App extends Component {
-  constructor () {
-    super();
-    this.state= {
-      value: ''
-    }
+// class App extends Component {
+//   constructor () {
+//     super();
+//     this.state= {
+//       value: ''
+//     }
 
-    this.handleChange = this.handleChange.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+//   handleChange(event) {
+//     this.setState({value: event.target.value});
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Un repetidor</h1>
+//         <input type="text" placeholder="Empieza a escribir algo" value={this.state.value} onChange={this.handleChange} />
+//         <p className="repeater">{this.state.value}</p>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [value, setValue] = useState('')
+
+  function handleChange(event) {
+    const {value} = event.target;
+    setValue(value)
   }
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-  render() {
-    return (
-      <div>
-        <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" value={this.state.value} onChange={this.handleChange} />
-        <p className="repeater">{this.state.value}</p>
-      </div>
-    );
-  }
+
+  return(
+    <div>
+      <h1>Un repetidor</h1>
+      <input type="text" placeholder="Empieza a escribir algo" value={value} onChange={handleChange} />
+      <p className="repeater">{value}</p>
+    </div>
+  )
 }
 
 export default App;
